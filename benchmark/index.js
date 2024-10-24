fac6e93d4f3479d3f26e45cb987c3dba09db0df6
Albert Martin <albert@imor.tl>
2024-10-24 22:00:21 +0000

    ci: make benchmarks runnable

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,43 +1,48 @@
-var iso3166 = require("../iso3166.min");
+const { Bench } = require('tinybench')
 
-suite("iso3166-2", function () {
-  bench("#country(alpha 2)", function () {
+const iso3166 = require('../iso3166.min')
+
+const bench = new Bench()
+
+bench
+  .add("#country(alpha 2)", function () {
     iso3166.country("SE");
-  });
+  })
 
-  bench("#country(alpha 3)", function () {
+  .add("#country(alpha 3)", function () {
     iso3166.country("USA");
-  });
+  })
 
-  bench("#subdivision(alpha 2, code)", function () {
+  .add("#subdivision(alpha 2, code)", function () {
     iso3166.country("SE", "O");
-  });
+  })
 
-  bench("#subdivision(alpha 3, code)", function () {
+  .add("#subdivision(alpha 3, code)", function () {
     iso3166.country("USA", "IN");
-  });
+  })
 
-  bench("#subdivision(alpha 2, name)", function () {
+  .add("#subdivision(alpha 2, name)", function () {
     iso3166.country("US", "Indiana");
-  });
+  })
 
-  bench("#subdivision(alpha 3, name)", function () {
+  .add("#subdivision(alpha 3, name)", function () {
     iso3166.country("USA", "Indiana");
-  });
+  })
 
-  bench("#subdivision(alpha 2-code)", function () {
+  .add("#subdivision(alpha 2-code)", function () {
     iso3166.country("SE-O");
-  });
+  })
 
-  bench("#subdivision(alpha 3-code)", function () {
+  .add("#subdivision(alpha 3-code)", function () {
     iso3166.country("SWE-O");
-  });
+  })
 
-  bench("#subdivision(alpha 2-name)", function () {
+  .add("#subdivision(alpha 2-name)", function () {
     iso3166.country("US-Indiana");
-  });
+  })
 
-  bench("#subdivision(alpha 3-name)", function () {
+  .add("#subdivision(alpha 3-name)", function () {
     iso3166.country("USA-Indiana");
   });
-});
+
+bench.run().then(() => console.table(bench.table()))
