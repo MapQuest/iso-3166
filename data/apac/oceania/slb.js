f7f74052893d7d13dcfa12d4be74dc5581cabafd
Albert Martin <albert@imor.tl>
2025-04-19 12:51:33 -0500

    feat(apac): add solomon islands

---
+++ data/apac/oceania/slb.js
@@ -0,0 +1,24 @@
+import Subdivisions from '../../subdivisions'
+
+export default {
+  country: 'Solomon Islands',
+  e164: 677,
+  fips: 'BP',
+  alpha2: 'SB',
+  alpha3: 'SLB',
+  ioc: 'SOL',
+  numeric: 90,
+  undp: 'SLB',
+  localities: new Map([
+    ['CE', ['Central', Subdivisions.PROVINCE]],
+    ['CH', ['Choiseul', Subdivisions.PROVINCE]],
+    ['CT', ['Capital Territory', Subdivisions.TERRITORY]],
+    ['GU', ['Guadalcanal', Subdivisions.PROVINCE]],
+    ['IS', ['Isabel', Subdivisions.PROVINCE]],
+    ['MK', ['Makira-Ulawa', Subdivisions.PROVINCE]],
+    ['ML', ['Malaita', Subdivisions.PROVINCE]],
+    ['RB', ['Rennell and Bellona', Subdivisions.PROVINCE]],
+    ['TE', ['Temotu', Subdivisions.PROVINCE]],
+    ['WE', ['Western', Subdivisions.PROVINCE]]
+  ])
+}
