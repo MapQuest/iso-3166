59c01d031a9096b776b3962b4996aa9aef2fd2e9
Albert Martin <albert@imor.tl>
2025-04-19 15:23:31 -0500

    feat(apac): add heard and mcdonald islands

---
+++ data/apac/oceania/hmd.js
@@ -0,0 +1,11 @@
+export default {
+  country: 'Heard Island and McDonald Islands',
+  e164: 672,
+  fips: 'HM',
+  alpha2: 'HM',
+  alpha3: 'HMD',
+  ioc: null,
+  numeric: 334,
+  undp: 'HMD',
+  localities: null
+}
