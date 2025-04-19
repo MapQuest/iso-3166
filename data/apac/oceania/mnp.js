f6988ba17f786faeb7569c9287bb10041a10074f
Albert Martin <albert@imor.tl>
2025-04-19 15:03:02 -0500

    feat(apac): add northern mariana islands

---
+++ data/apac/oceania/mnp.js
@@ -0,0 +1,18 @@
+import Subdivisions from '../../subdivisions'
+
+export default {
+  country: 'Northern Mariana Islands',
+  e164: 1670,
+  fips: 'CQ',
+  alpha2: 'MP',
+  alpha3: 'MNP',
+  ioc: 'NMA',
+  numeric: 580,
+  undp: 'MNP',
+  localities: new Map([
+    ['100', ['Rota', Subdivisions.MUNICIPALITY]],
+    ['110', ['Saipan', Subdivisions.MUNICIPALITY]],
+    ['120', ['Tinian', Subdivisions.MUNICIPALITY]],
+    ['010', ['Northern Islands', Subdivisions.MUNICIPALITY]]
+  ])
+}
