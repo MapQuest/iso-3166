61148dfa69b46abc88e3c05d6ce0d96e205cbd5e
Albert Martin <albert@imor.tl>
2025-04-19 12:10:42 -0500

    feat(apac): add singapore

---
+++ data/apac/asia/sgp.js
@@ -0,0 +1,19 @@
+import Subdivisions from '../../subdivisions'
+
+export default {
+  country: 'Singapore',
+  e164: 65,
+  fips: 'SN',
+  alpha2: 'SG',
+  alpha3: 'SGP',
+  ioc: 'SGP',
+  numeric: 702,
+  undp: 'SGP',
+  localities: new Map([
+    ['01', ['Central Singapore', Subdivisions.DISTRICT]],
+    ['02', ['North East', Subdivisions.DISTRICT]],
+    ['03', ['North West', Subdivisions.DISTRICT]],
+    ['04', ['South East', Subdivisions.DISTRICT]],
+    ['05', ['South West', Subdivisions.DISTRICT]]
+  ])
+}
