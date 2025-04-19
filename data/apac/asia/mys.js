6b7fa2aa21953dbd06adc1e2467e4df14713aa58
Albert Martin <albert@imor.tl>
2025-04-19 12:09:51 -0500

    feat(apac): add malaysia

---
+++ data/apac/asia/mys.js
@@ -0,0 +1,30 @@
+import Subdivisions from '../../subdivisions'
+
+export default {
+  country: 'Malaysia',
+  e164: 60,
+  fips: 'MY',
+  alpha2: 'MY',
+  alpha3: 'MYS',
+  ioc: 'MAS',
+  numeric: 458,
+  undp: 'MYS',
+  localities: new Map([
+    ['01', ['Johor', Subdivisions.STATE]],
+    ['02', ['Kedah', Subdivisions.STATE]],
+    ['03', ['Kelantan', Subdivisions.STATE]],
+    ['04', ['Melaka', Subdivisions.STATE]],
+    ['05', ['Negeri Sembilan', Subdivisions.STATE]],
+    ['06', ['Pahang', Subdivisions.STATE]],
+    ['07', ['Pulau Pinang', Subdivisions.STATE]],
+    ['08', ['Perak', Subdivisions.STATE]],
+    ['09', ['Perlis', Subdivisions.STATE]],
+    ['10', ['Selangor', Subdivisions.STATE]],
+    ['11', ['Terengganu', Subdivisions.STATE]],
+    ['12', ['Sabah', Subdivisions.STATE]],
+    ['13', ['Sarawak', Subdivisions.STATE]],
+    ['14', ['Wilayah Persekutuan Kuala Lumpur', Subdivisions.TERRITORY]],
+    ['15', ['Wilayah Persekutuan Labuan', Subdivisions.TERRITORY]],
+    ['16', ['Wilayah Persekutuan Putrajaya', Subdivisions.TERRITORY]]
+  ])
+}
