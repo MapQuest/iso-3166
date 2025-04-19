dbb9d140bfc65c18079cdadaa2f3c5b50b56e92e
Albert Martin <albert@imor.tl>
2025-04-19 15:37:47 -0500

    feat(apac): add turkmenistan

---
+++ data/apac/asia/tkm.js
@@ -0,0 +1,20 @@
+import Subdivisions from '../../subdivisions'
+
+export default {
+  country: 'Turkmenistan',
+  e164: 993,
+  fips: 'TX',
+  alpha2: 'TM',
+  alpha3: 'TKM',
+  ioc: 'TKM',
+  numeric: 795,
+  undp: 'TKM',
+  localities: new Map([
+    ['A', ['Ahal', Subdivisions.REGION]],
+    ['B', ['Balkan', Subdivisions.REGION]],
+    ['D', ['Dashoguz', Subdivisions.REGION]],
+    ['L', ['Lebap', Subdivisions.REGION]],
+    ['M', ['Mary', Subdivisions.REGION]],
+    ['S', ['Ashgabat', Subdivisions.CITY]]
+  ])
+}
