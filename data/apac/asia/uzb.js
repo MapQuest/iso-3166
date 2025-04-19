1413b5a727036230cb29d52463f87c376acedfc6
Albert Martin <albert@imor.tl>
2025-04-19 15:36:09 -0500

    feat(apac): add uzbekistan

---
+++ data/apac/asia/uzb.js
@@ -0,0 +1,28 @@
+import Subdivisions from '../../subdivisions'
+
+export default {
+  country: 'Uzbekistan',
+  e164: 998,
+  fips: 'UZ',
+  alpha2: 'UZ',
+  alpha3: 'UZB',
+  ioc: 'UZB',
+  numeric: 860,
+  undp: 'UZB',
+  localities: new Map([
+    ['AN', ['Andijan', Subdivisions.REGION]],
+    ['BU', ['Bukhara', Subdivisions.REGION]],
+    ['FA', ['Fergana', Subdivisions.REGION]],
+    ['JI', ['Jizzakh', Subdivisions.REGION]],
+    ['KH', ['Karakalpakstan', Subdivisions.AUTONOMOUS_REGION]],
+    ['NG', ['Namangan', Subdivisions.REGION]],
+    ['NW', ['Navoiy', Subdivisions.REGION]],
+    ['QA', ['Kashkadarya', Subdivisions.REGION]],
+    ['SA', ['Samarkand', Subdivisions.REGION]],
+    ['SI', ['Sirdaryo', Subdivisions.REGION]],
+    ['SU', ['Surkhandarya', Subdivisions.REGION]],
+    ['TK', ['Tashkent', Subdivisions.REGION]],
+    ['TO', ['Tashkent', Subdivisions.CITY]],
+    ['XO', ['Khorezm', Subdivisions.REGION]]
+  ])
+}
