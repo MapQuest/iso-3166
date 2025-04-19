017638834789bde0972c56fc1b59837a64ab31ed
Albert Martin <albert@imor.tl>
2025-04-19 12:54:50 -0500

    feat(apac): add samoa

---
+++ data/apac/oceania/wsm.js
@@ -0,0 +1,25 @@
+import Subdivisions from '../../subdivisions'
+
+export default {
+  country: 'Samoa',
+  e164: 685,
+  fips: 'WS',
+  alpha2: 'WS',
+  alpha3: 'WSM',
+  ioc: 'SAM',
+  numeric: 882,
+  undp: 'WSM',
+  localities: new Map([
+    ['AA', ['A\'ana', Subdivisions.DISTRICT]],
+    ['AL', ['Aiga-i-le-Tai', Subdivisions.DISTRICT]],
+    ['AT', ['Atua', Subdivisions.DISTRICT]],
+    ['FA', ['Fa\'asaleleaga', Subdivisions.DISTRICT]],
+    ['GE', ['Gaga\'emauga', Subdivisions.DISTRICT]],
+    ['GI', ['Gaga\'ifomauga', Subdivisions.DISTRICT]],
+    ['PA', ['Palauli', Subdivisions.DISTRICT]],
+    ['SA', ['Satupa\'itea', Subdivisions.DISTRICT]],
+    ['TU', ['Tuamasaga', Subdivisions.DISTRICT]],
+    ['VF', ['Vaisigano', Subdivisions.DISTRICT]],
+    ['VS', ['Va\'a-o-Fonoti', Subdivisions.DISTRICT]]
+  ])
+}
