2d202c7619f2df370ca37020abd10edf0d891038
Albert Martin <albert@imor.tl>
2025-04-19 12:56:47 -0500

    feat(apac): add tonga

---
+++ data/apac/oceania/ton.js
@@ -0,0 +1,19 @@
+import Subdivisions from '../../subdivisions'
+
+export default {
+  country: 'Tonga',
+  e164: 676,
+  fips: 'TN',
+  alpha2: 'TO',
+  alpha3: 'TON',
+  ioc: 'TGA',
+  numeric: 776,
+  undp: 'TON',
+  localities: new Map([
+    ['01', ['Eua', Subdivisions.DIVISION]],
+    ['02', ['Ha\'apai', Subdivisions.DIVISION]],
+    ['03', ['Niuas', Subdivisions.DIVISION]],
+    ['04', ['Tongatapu', Subdivisions.DIVISION]],
+    ['05', ['Vava\'u', Subdivisions.DIVISION]]
+  ])
+}
