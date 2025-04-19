381217062b8096ea06910bef10e2611a44155447
Albert Martin <albert@imor.tl>
2025-04-19 15:24:51 -0500

    feat(apac): add british indian ocean territory

---
+++ data/apac/oceania/iot.js
@@ -0,0 +1,11 @@
+export default {
+  country: 'British Indian Ocean Territory',
+  e164: 246,
+  fips: 'IO',
+  alpha2: 'IO',
+  alpha3: 'IOT',
+  ioc: null,
+  numeric: 86,
+  undp: 'IOT',
+  localities: null
+}
